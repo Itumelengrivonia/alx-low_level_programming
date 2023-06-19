ca75c272dcd920beae9149a1e92b27e7898f76ff
itumelengrivonia <itumelengrivonia@gmail.com>
2023-06-18 19:56:00 -0700

    C Variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+insclude <string.h>
+
+/**
+ * main - Prints the alphabet withoutq and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != m 101 m && i != 123)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
