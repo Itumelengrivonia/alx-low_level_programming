36ecf3fc69a9fac8b9f650d74b5c01898cd03214
itumelengrivonia <itumelengrivonia@gmail.com>
2023-06-29 11:26:42 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,13 +7,13 @@
  * Return: Nothing
  */
 
- char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 
 	int i;
 	int j;
 
-	i =0;
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -21,7 +21,7 @@
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest [i] = src[j]:
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
